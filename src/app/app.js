938901aee8e4e60d811b287ef831fbc4826308b5
Hamza Waqas <hamza.waqas@tenpearls.com>
2015-09-11 00:34:26 +0500

    removed james-saay (tmp)

--- src/app/app.js
+++ src/app/app.js
@@ -5,7 +5,6 @@ var React = require('react'),
     Route  = Router.Route,
     RouteHandler = Router.RouteHandler,
     Bootstrap = require('react-bootstrap'),
-    james = require('james-say'),
     Nav = Bootstrap.Nav,
     NavItem = Bootstrap.NavItem,
     WynkApp;

5af35201fe024fb0c7ef5f51a83e0ffe770f7ead
Haza <hamzawaqas.programmer@gmail.com>
2015-09-29 13:03:27 +0000

    added dist/

--- shipitfile.js
+++ shipitfile.js
@@ -1,6 +1,4 @@
 
-var Async = require('async');
-
 module.exports = function (shipit) {
   require('shipit-deploy')(shipit);
   // require('shipit-shared')(shipit);
@@ -9,52 +7,17 @@ module.exports = function (shipit) {
     default: {
       workspace: '/tmp/osler-ui',
       deployTo: '~/code/osler-ui',
-      repositoryUrl: 'https://github.com/10pearls/GuestMetrics-Node.git',
+      repositoryUrl: 'https://github.com/wynkk/osler-ui.git',
       ignores: ['.git', 'node_modules'],
       rsync: ['--del'],
       keepReleases: 3,
       // key: '/path/to/key',
       shallowClone: true
     },
-    qa: {
-      servers: 'ubuntu@ec2-54-211-192-158.compute-1.amazonaws.com',
-      key: '/var/www/keys/GuestMetrics-dev.pem',
-      branch: 'develop'
-    },
-    staging: {
-      servers: 'ubuntu@ec2-54-204-156-226.compute-1.amazonaws.com',
-      key: '/var/www/keys/GuestMetrics-staging.pem',
-      branch: 'dev-master'
-    },
-    production: {
+    demo: {
+      servers: 'deploy@demo.wynkk.co',
+      // key: '/var/www/keys/GuestMetrics-dev.pem',
       branch: 'master'
     }
-  });
-  
-  /**
-  * Execute the remote script to start the server
-  */
-  shipit.task('start-server', function startServer(done) {
-    // shipit.log();
-    // Start the server here.
-    Async.series({
-      exportEnvironment: function exportEnvironment(callback) {
-        shipit.remote('export NODE_ENV=' + shipit.environment, callback);
-      },
-      stopExistingProcesses: function stopExistingProcesses(callback) {
-        return callback();
-      },
-    }, function result(err, result) {
-      if (err) return done(err);
-      shipit.remote('/bin/sh -c ~/guestmetrics/current/bin/start.sh');
-    });
-    // return shipit.remote('/bin/sh -c ~/guestmetrics/current/bin/start.sh');
-  });
-  
-  /**
-  * Once `cleaned`, restart the server or start it.
-  */
-  shipit.on('cleaned', function startApp() {
-    shipit.start('start-server');
   });
 };
